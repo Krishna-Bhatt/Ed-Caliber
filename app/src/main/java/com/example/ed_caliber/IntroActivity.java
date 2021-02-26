0e17f1ffc781c88f78e0667eb6034dc3e1fc65fb
Admin <bhattkrishna605@gmail.com>
2021-02-26 22:03:22 +0530

    Matrices Initiated

--- app/src/main/java/com/example/ed_caliber/IntroActivity.java
+++ app/src/main/java/com/example/ed_caliber/IntroActivity.java
@@ -1,8 +1,21 @@
 package com.example.ed_caliber;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.MediaController;
+import android.widget.Toast;
+import android.widget.VideoView;
 
 public class IntroActivity extends android.app.Activity {
 
@@ -10,5 +23,30 @@ public class IntroActivity extends android.app.Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+        ImageView prev = (ImageView) findViewById(R.id.prev);
+        VideoView videoView =(VideoView)findViewById(R.id.intro_video);
+        videoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.intro_mute));
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+        videoView.start();
+
+
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                Intent intent=new  Intent(IntroActivity.this,StartPageActivity.class);
+                startActivity(intent);
+
+            }
+        });
+        prev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(IntroActivity.this,ChooseActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
