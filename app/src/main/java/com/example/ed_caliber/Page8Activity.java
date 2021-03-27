a4007a4e8a4805b3f6e609c352672a53b1d9e963
Admin <bhattkrishna605@gmail.com>
2021-03-27 12:38:38 +0530

    Page9 completed

--- app/src/main/java/com/example/ed_caliber/Page8Activity.java
+++ app/src/main/java/com/example/ed_caliber/Page8Activity.java
@@ -1,12 +1,32 @@
 package com.example.ed_caliber;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Page8Activity extends android.app.Activity {
-
+    ImageView prev,next;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page8);
+        prev = (ImageView) findViewById(R.id.prev);
+        next = (ImageView) findViewById(R.id.next);
+        prev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Page8Activity.this, Page7Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Page8Activity.this, Page9Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/ed_caliber/Page9Activity.java
@@ -0,0 +1,14 @@
+package com.example.ed_caliber;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Page9Activity extends android.app.Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page9);
+    }
+}
